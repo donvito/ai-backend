293aeab0d1b3bcb29d78bb3c885550e3536c91b0
Melvin Vivas <melvindave@gmail.com>
2025-05-22 14:42:56 +0800

    feat: update project configuration to use TypeScript, add dotenv for environment variables, and enhance server setup with Hono

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';  
+import { serve } from '@hono/node-server'
 import initialise from './utils/initialise' 
 import configureRoutes from './utils/apiRoutes'
 import configureApiDocs from './utils/apiDocs'
@@ -10,4 +12,10 @@ configureRoutes(app)
 // Configure the API docs
 configureApiDocs(app)
 
-export default app
+const port = process.env.PORT || 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port: Number(port)
+})
